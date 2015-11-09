592893d496a1ba937a8af161a7cb749f1c89b362
George Mathew <george.meg91@gmail.com>
2015-11-09 12:42:26 -0500

    Dot

--- softgoal2004/code/costs1.pl
+++
@@ -1,8 +0,0 @@
-costs(X) :- todos(All),length(All,X),!.
-costs(0).
-
-todos(All) :- bagof(X,todo(X),All),!.
-todo(X) :- memo(_,X,0), 
-  \+ clause(node_info(claim, X, _), true), 
-  \+ clause(node_info(stakeholder, X, _), true), 
-  \+ X=not(_). 
--- softgoal2004/code/tar2/runtar2_arch_patterns.bat
+++
@@ -1,7 +0,0 @@
-tar2 arch_patterns > arch_patterns.out
-
-REM tar2 -r arch_patterns > arch_patterns_r.out
-
-REM xval tar2 arch_patterns 10
-pause
-
